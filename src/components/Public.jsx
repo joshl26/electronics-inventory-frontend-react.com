c57a20e45308bcd868ef554f2dbfe7b25f98350e
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-05 11:54:47 -0500

    commit

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import React from "react";
 import LoginHeader from "../features/pages/LoginHeader";
 
--- src/index.jsx
+++ src/index.jsx
@@ -6,18 +6,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <ThemeProvider
+    breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    minBreakpoint="xxs"
+  >
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </ThemeProvider>
 );
