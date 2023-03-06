39300fd4b3e8f0974394e29d87ebbaf42ee558ed
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-06 12:20:43 -0500

    commit

--- src/features/pages/LoginHeader.jsx
+++ src/features/pages/LoginHeader.jsx
@@ -1,16 +1,19 @@
-import "./LoginHeader.css";
+import "./LoginHeader.scss";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import brandIcon from "../../img/eiicon.png";
 
 const LoginHeader = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Container className="navbar_container">
+          <Navbar.Brand href="#home">
+            <img className="header_icon" src={brandIcon} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
@@ -33,7 +36,7 @@ const LoginHeader = () => {
             <Nav>
               <Nav.Link href="/login">Employee Login</Nav.Link>
               <Nav.Link eventKey={2} href="/">
-                Signup
+                New User Signup
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
