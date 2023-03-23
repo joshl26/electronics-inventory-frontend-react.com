879d55810387a4f96c9e23e7b45256aba119c8fa
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-22 22:21:43 -0400

    react-router-6.90-update

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PersistLogin";
+import RequireAuth from "./features/auth/RequireAuth";
+import { ROLES } from "./config/roles";
+import ErrorPage from "./error-page";
+
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      {/* public routes */}
+      <Route index element={<Public />} />
+      <Route path="login" element={<Login />} />
+
+      {/* Protected Routes */}
+      <Route element={<PersistLogin />}>
+        <Route
+          element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+        >
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
+              <Route
+                element={
+                  <RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />
+                }
+              >
+                <Route path="users">
+                  <Route index element={<UsersList />} />
+                  <Route path=":id" element={<EditUser />} />
+                  <Route path="new" element={<NewUserForm />} />
+                </Route>
+              </Route>
+
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
+            </Route>
+            {/* End Dash */}
+          </Route>
+        </Route>
+      </Route>
+      {/* End Protected Routes */}
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,12 +81,9 @@ root.render(
   >
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </Provider>
     </React.StrictMode>
   </ThemeProvider>
 );
+
