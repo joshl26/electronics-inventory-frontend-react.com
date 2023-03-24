c27de427e0719a58e12310fe1a87d13cbaa2b84c
Joshua Lehman <joshlehman.dev@gmail.com>
2023-03-24 11:30:28 -0400

    cleanup

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -36,6 +36,7 @@ const DashHeader = () => {
   const onNewUserClicked = () => navigate("/dash/users/new");
   const onNotesClicked = () => navigate("/dash/notes");
   const onUsersClicked = () => navigate("/dash/users");
+  const onHomeClicked = () => navigate("/dash");
 
   let dashClass = null;
   if (
@@ -92,6 +93,30 @@ const DashHeader = () => {
     );
   }
 
+  const homeButton = (
+    <button
+      class="dash-footer__button icon-button"
+      onClick={onHomeClicked}
+      title="Home"
+    >
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="house"
+        class="svg-inline--fa fa-house "
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+      >
+        <path
+          fill="currentColor"
+          d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
+        ></path>
+      </svg>
+    </button>
+  );
+
   const logoutButton = (
     <button className="icon-button" title="Logout" onClick={sendLogout}>
       <FontAwesomeIcon icon={faRightFromBracket} />
@@ -106,6 +131,7 @@ const DashHeader = () => {
   } else {
     buttonContent = (
       <>
+        {homeButton}
         {newNoteButton}
         {newUserButton}
         {notesButton}
@@ -116,20 +142,19 @@ const DashHeader = () => {
   }
 
   const content = (
-    <>
-      <p className={errClass}>{error?.data?.message}</p>
-
-      <header className="dash-header">
-        <div className={`dash-header__container ${dashClass}`}>
+    <header className="dash-header">
+      <div className={`dash-header__container ${dashClass}`}>
+        {error ? <p className={errClass}>{error?.data?.message}</p> : ""}
+        <div className="dash-header-link">
           <Link to="/dash">
             <h1 className="dash-header__title">
               Electronics Inventory Dashboard
             </h1>
           </Link>
-          <nav className="dash-header__nav">{buttonContent}</nav>
         </div>
-      </header>
-    </>
+        <nav className="dash-header__nav">{buttonContent}</nav>
+      </div>
+    </header>
   );
 
   return content;
