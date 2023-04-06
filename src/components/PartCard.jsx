624c8f6014b643a14dd5d197c7dedaa84fce7438
Joshua Lehman <joshlehman.dev@gmail.com>
2023-04-06 01:45:38 -0400

    part-card-html

--- src/components/PartCard.jsx
+++ src/components/PartCard.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import classes from "./PartCard.module.scss";
 import { useSelector } from "react-redux";
 import { selectPartById } from "../features/parts/partsApiSlice";
+import { Row, Col, Container } from "react-bootstrap";
 
 const PartCard = ({ partId }) => {
   const part = useSelector((state) => selectPartById(state, partId));
 
   const partImages = part.images.map((image, idx) => {
     return (
-      <div key={idx}>
+      <div key={image._id}>
         <a href={image.url}>
           <img className={classes.partcard_image} src={image.url} />
         </a>
@@ -18,27 +19,121 @@ const PartCard = ({ partId }) => {
 
   return (
     <div key={part._id} className={classes.partcard_container}>
-      <p className={classes.partid_header}></p>
-      <p className={classes.partid_text}>{part.id}</p>
-      <h1 className={classes.partname_header}>Part Name</h1>
-      <h1 className={classes.partname_text}>{part.name}</h1>
-      <h2 className={classes.partdescription_header}>Description</h2>
-      <h3 className={classes.partdescription_text}>{part.description}</h3>
-      <h2 className={classes.partuser_header}>Creator</h2>
-      <h3 className={classes.partuser_text}>{part.username}</h3>
-      <h2 className={classes.partqty_header}>Qty</h2>
-      <h3 className={classes.partqty_text}>{part.qty}</h3>
-      <h2 className={classes.parttype_header}>Part Type</h2>
-      <h3 className={classes.parttype_text}>{part.partType}</h3>
-      <h2 className={classes.partcreated_header}>Date Created</h2>
-      <h3 className={classes.partcreated_text}>{part.createdAt}</h3>
-      <h2 className={classes.partupdated_header}>Date Updated</h2>
-      <h3 className={classes.partupdated_text}>{part.updatedAt}</h3>
-      <h2 className={classes.partlocation_header}>Location</h2>
-      <h3 className={classes.partlocation_text}>{part.location}</h3>
-      <h2 className={classes.partpackage_header}>Package</h2>
-      <h3 className={classes.partpackage_text}>{part.package}</h3>
-      {partImages}
+      <Container>
+        <Row>
+          <Col>
+            <Row>
+              <Col>
+                <h1 className={`classes.partname_header ${classes.text}`}>
+                  Part Name:
+                </h1>
+              </Col>
+              <Col>
+                <h1 className={`classes.partname_text ${classes.text}`}>
+                  {part.name}
+                </h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h2 className={`classes.parttype_header ${classes.text}`}>
+                  Part Type
+                </h2>
+              </Col>
+
+              <Col>
+                <h3 className={`classes.parttype_text ${classes.text}`}>
+                  {part.partType}
+                </h3>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <div key={part.images[0]._id}>
+                  <a href={part.images[0].url}>
+                    <img
+                      className={classes.partcard_image}
+                      src={part.images[0].url}
+                    />
+                  </a>
+                </div>
+              </Col>
+              <Col>
+                <div key={part.images[1]._id}>
+                  <a href={part.images[1].url}>
+                    <img
+                      className={classes.partcard_image}
+                      src={part.images[1].url}
+                    />
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className={`classes.partdescription_header ${classes.text}`}>
+              Description
+            </h2>
+            <h3 className={`classes.partdescription_text ${classes.text}`}>
+              {part.description}
+            </h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className={`classes.partpackage_header ${classes.text}`}>
+              Package
+            </h2>
+            <h3 className={`classes.partpackage_text ${classes.text}`}>
+              {part.package}
+            </h3>
+          </Col>
+          <Col>
+            <h2 className={`classes.partlocation_header ${classes.text}`}>
+              Location
+            </h2>
+            <h3 className={`classes.partlocation_text ${classes.text}`}>
+              {part.location}
+            </h3>
+          </Col>
+          <Col>
+            <h2 className={`classes.partqty_header ${classes.text}`}>Qty</h2>
+            <h3 className={`classes.partqty_text ${classes.text}`}>
+              {part.qty}
+            </h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className={`classes.partupdated_header ${classes.text}`}>
+              Last Updated
+            </h2>
+            <h3 className={`classes.partupdated_text ${classes.text}`}>
+              {part.updatedAt}
+            </h3>
+          </Col>
+          <Col>
+            <h2 className={`classes.partcreated_header ${classes.text}`}>
+              Date Created
+            </h2>
+            <h3 className={`classes.partcreated_text ${classes.text}`}>
+              {part.createdAt}
+            </h3>
+          </Col>{" "}
+          <Col>
+            <h2 className={`classes.partcreator_header ${classes.text}`}>
+              Creator
+            </h2>
+            <h3 className={`classes.partcreator_text ${classes.text}`}>
+              {part.username}
+            </h3>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
