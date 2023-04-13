187c038b8b3670f8d2bdd1a86d0218beaf3d2b2c
Joshua Lehman <joshlehman.dev@gmail.com>
2023-04-12 23:38:26 -0400

    login link fix

--- src/features/pages/LoginHeader.jsx
+++ src/features/pages/LoginHeader.jsx
@@ -5,6 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Lottie from "lottie-react";
 import HamburgerMenu from "../../svg/HamburgerMenu.json";
+import { Link } from "react-router-dom";
 
 const LoginHeader = () => {
   return (
@@ -34,7 +35,9 @@ const LoginHeader = () => {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="/login">User Login</Nav.Link>
+            <Nav.Link>
+              <Link to="/login"> User Login</Link>
+            </Nav.Link>
             <Nav.Link eventKey={2} href="/">
               Signup
             </Nav.Link>
