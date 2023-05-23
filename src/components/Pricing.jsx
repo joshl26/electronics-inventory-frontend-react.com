08b098ebdd892666abd084f215cf6f794cc79bcb
Joshua Lehman <joshlehman.dev@gmail.com>
2023-05-23 17:00:19 -0400

    pricing_table

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -65,28 +65,36 @@ const Pricing = () => {
           <div className="spacer"></div>
 
           <Row>
-            <Col xs={1} md={4} className="pricing-table-col">
+            <Col xs={1} md={3} className="pricing-table-col">
               <Row>
-                <h1>Free</h1>
+                <div className="spacer-x-small"></div>
+
+                <h4>FREE</h4>
               </Row>
               <Row>
+                <div className="spacer-x-small"></div>
+
                 <h1>$0USD</h1>
               </Row>
               <Row>
-                <h1>Free for your whole team</h1>
+                <p>Free for your whole team</p>
+                <div className="spacer-x-small"></div>
               </Row>
               <Row>
-                <h1>
+                <h4>
                   For individuals or teams looking to organize any project.
-                </h1>
+                </h4>
               </Row>
+              <div className="spacer"></div>
+
               <Row>
                 <div>
                   <Button>Get Started</Button>
                 </div>
               </Row>
+              <div className="spacer-small"></div>
               <Row classname="row-border-top">
-                <h3>INCLUDED IN FREE:</h3>
+                <h4>INCLUDED IN FREE:</h4>
               </Row>
               <Row classname="row-border-top">
                 <Col xs={1} md={1}>
@@ -136,36 +144,12 @@ const Pricing = () => {
                   <p>Unlimited activity log</p>
                 </Col>
               </Row>
-            </Col>
-            <Col xs={1} md={4} className="pricing-table-col">
-              <Row>
-                <h1>Free</h1>
-              </Row>
-              <Row>
-                <h1>$0USD</h1>
-              </Row>
-              <Row>
-                <h1>Free for your whole team</h1>
-              </Row>
-              <Row>
-                <h1>
-                  For individuals or teams looking to organize any project.
-                </h1>
-              </Row>
-              <Row>
-                <div>
-                  <Button>Get Started</Button>
-                </div>
-              </Row>
-              <Row classname="row-border-top">
-                <h3>INCLUDED IN FREE:</h3>
-              </Row>
               <Row classname="row-border-top">
                 <Col xs={1} md={1}>
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Unlimited cards</p>
+                  <p>Assignee and due dates</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -173,7 +157,7 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Up to 10 boards per Workspace</p>
+                  <p>iOS and Android mobile apps</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -181,72 +165,51 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Unlimited storage (10MB/file)</p>
-                </Col>
-              </Row>
-              <Row classname="row-border-top">
-                <Col xs={1} md={1}>
-                  <p>✓</p>
-                </Col>
-                <Col xs={1} md={11}>
-                  <p>250 Workspace command runs per month</p>
-                </Col>
-              </Row>
-              <Row classname="row-border-top">
-                <Col xs={1} md={1}>
-                  <p>✓</p>
-                </Col>
-                <Col xs={1} md={11}>
-                  <p>Custom backgrounds & stickers</p>
-                </Col>
-              </Row>
-              <Row classname="row-border-top">
-                <Col xs={1} md={1}>
-                  <p>✓</p>
-                </Col>
-                <Col xs={1} md={11}>
-                  <p>Unlimited activity log</p>
+                  <p>2-factor authentication</p>
                 </Col>
               </Row>
             </Col>
 
-            <Col xs={1} md={4} className="pricing-table-col">
+            {/* ---------------------------------------------------------------------------------------- */}
+
+            <Col xs={1} md={3} className="pricing-table-col">
               <Row>
-                <h1>Free</h1>
+                <div className="spacer-x-small"></div>
+
+                <h4>STANDARD</h4>
               </Row>
               <Row>
-                <h1>$0USD</h1>
+                <div className="spacer-x-small"></div>
+
+                <h1>$5USD</h1>
               </Row>
               <Row>
-                <h1>Free for your whole team</h1>
+                <p>Per user/month if billed annually ($6 billed monthly)</p>
+                <div className="spacer-x-small"></div>
               </Row>
               <Row>
-                <h1>
-                  For individuals or teams looking to organize any project.
-                </h1>
+                <h4>
+                  For small teams that need to manage work and scale
+                  collaboration.
+                </h4>
               </Row>
+              <div className="spacer"></div>
+
               <Row>
                 <div>
-                  <Button>Get Started</Button>
+                  <Button>Sign up now</Button>
                 </div>
               </Row>
+              <div className="spacer-small"></div>
               <Row classname="row-border-top">
-                <h3>INCLUDED IN FREE:</h3>
+                <h4>EVERYTHING IN FREE, PLUS:</h4>
               </Row>
               <Row classname="row-border-top">
                 <Col xs={1} md={1}>
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Unlimited cards</p>
-                </Col>
-              </Row>
-              <Row classname="row-border-top">
-                <Col xs={1} md={1}>
-                  <p>✓</p>
-                </Col>
-                <Col xs={1} md={11}>
-                  <p>Up to 10 boards per Workspace</p>
+                  <p>Unlimited boards</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -254,7 +217,7 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Unlimited storage (10MB/file)</p>
+                  <p>Advanced checklists</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -262,7 +225,7 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>250 Workspace command runs per month</p>
+                  <p>Custom Fields</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -270,7 +233,7 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Custom backgrounds & stickers</p>
+                  <p>Unlimited storage (250MB/file)</p>
                 </Col>
               </Row>
               <Row classname="row-border-top">
@@ -278,7 +241,227 @@ const Pricing = () => {
                   <p>✓</p>
                 </Col>
                 <Col xs={1} md={11}>
-                  <p>Unlimited activity log</p>
+                  <p>1,000 Workspace command runs per month</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Single board guests</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Saved searches</p>
+                </Col>
+              </Row>
+            </Col>
+
+            {/* ---------------------------------------------------------------------------------------- */}
+
+            <Col xs={1} md={3} className="pricing-table-col-special">
+              <Row>
+                <div className="spacer-x-small"></div>
+
+                <h4>PREMIUM</h4>
+              </Row>
+              <Row>
+                <div className="spacer-x-small"></div>
+
+                <h1>$10USD</h1>
+              </Row>
+              <Row>
+                <p>Per user/month if billed annually ($12.50 billed monthly)</p>
+                <div className="spacer-x-small"></div>
+              </Row>
+              <Row>
+                <h4>
+                  For teams that need to track and visualize multiple projects
+                  in several ways, including boards, timelines, calendars, etc.
+                </h4>
+              </Row>
+              <div className="spacer"></div>
+
+              <Row>
+                <div>
+                  <Button>Try for free</Button>
+                </div>
+              </Row>
+              <div className="spacer-small"></div>
+              <Row classname="row-border-top">
+                <h4>EVERYTHING IN STANDARD, PLUS:</h4>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Views: Calendar, Timeline, Table, Dashboard, and Map</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Workspace views: Table and Calendar</p>
+                </Col>
+              </Row>
+
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited Workspace command runs</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Admin and security features</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Workspace-level templates</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Collections</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Observers</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Simple data export</p>
+                </Col>
+              </Row>
+            </Col>
+
+            {/* ---------------------------------------------------------------------------------------- */}
+
+            <Col xs={1} md={3} className="pricing-table-col">
+              <Row>
+                <div className="spacer-x-small"></div>
+
+                <h4>ENTERPRISE</h4>
+              </Row>
+              <Row>
+                <div className="spacer-x-small"></div>
+
+                <h1>$17.5USD</h1>
+              </Row>
+              <Row>
+                <p>
+                  Per user/month - billed annually ($210.00 annual price per
+                  user)
+                </p>
+                <div className="spacer-x-small"></div>
+              </Row>
+              <Row>
+                <h4>
+                  For organizations that need to connect work across teams with
+                  more security and controls.
+                </h4>
+              </Row>
+              <div className="spacer"></div>
+
+              <Row>
+                <div>
+                  <Button>Contact Sales</Button>
+                </div>
+              </Row>
+              <div className="spacer-small"></div>
+              <Row classname="row-border-top">
+                <h4>EVERYTHING IN PREMIUM, PLUS:</h4>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited Workspaces</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Organization-wide permissions</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Organization-visible boards</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Public board management</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Multi-board guests</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Attachment permissions</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Power-Up administration</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Free SSO and user provisioning with Atlassian Access</p>
                 </Col>
               </Row>
             </Col>
