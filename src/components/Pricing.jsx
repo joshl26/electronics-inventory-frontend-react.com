26eb918ffd985e368f38e8e332a56d272c174c5c
Joshua Lehman <joshlehman.dev@gmail.com>
2023-05-23 16:28:52 -0400

    pricing_page_updates

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -8,6 +8,7 @@ import SwipeUpIcon from "../svg/SwipeUpIcon.json";
 import Lottie from "lottie-react";
 import CustomerGallery from "../features/pages/CustomerGallery";
 import { useEffect, useCallback } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Pricing = () => {
   const [colorMode, setColorMode] = useState(
@@ -55,8 +56,238 @@ const Pricing = () => {
       <section className={pricingStyle}>
         <div className="spacer"></div>
 
-        <h1 classname="pricing-header-text">Pricing</h1>
+        <Container>
+          <h1 classname="pricing-h1-text">Electronics Inventory YOUR WAY.</h1>
+          <h4 classname="pricing-h4-text">
+            Trusted by millions, Electronics Inventory powers teams all around
+            the world. Explore which option is right for you.
+          </h4>
+          <div className="spacer"></div>
+
+          <Row>
+            <Col xs={1} md={4} className="pricing-table-col">
+              <Row>
+                <h1>Free</h1>
+              </Row>
+              <Row>
+                <h1>$0USD</h1>
+              </Row>
+              <Row>
+                <h1>Free for your whole team</h1>
+              </Row>
+              <Row>
+                <h1>
+                  For individuals or teams looking to organize any project.
+                </h1>
+              </Row>
+              <Row>
+                <div>
+                  <Button>Get Started</Button>
+                </div>
+              </Row>
+              <Row classname="row-border-top">
+                <h3>INCLUDED IN FREE:</h3>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited cards</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Up to 10 boards per Workspace</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited storage (10MB/file)</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>250 Workspace command runs per month</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Custom backgrounds & stickers</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited activity log</p>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={1} md={4} className="pricing-table-col">
+              <Row>
+                <h1>Free</h1>
+              </Row>
+              <Row>
+                <h1>$0USD</h1>
+              </Row>
+              <Row>
+                <h1>Free for your whole team</h1>
+              </Row>
+              <Row>
+                <h1>
+                  For individuals or teams looking to organize any project.
+                </h1>
+              </Row>
+              <Row>
+                <div>
+                  <Button>Get Started</Button>
+                </div>
+              </Row>
+              <Row classname="row-border-top">
+                <h3>INCLUDED IN FREE:</h3>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited cards</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Up to 10 boards per Workspace</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited storage (10MB/file)</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>250 Workspace command runs per month</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Custom backgrounds & stickers</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited activity log</p>
+                </Col>
+              </Row>
+            </Col>
+
+            <Col xs={1} md={4} className="pricing-table-col">
+              <Row>
+                <h1>Free</h1>
+              </Row>
+              <Row>
+                <h1>$0USD</h1>
+              </Row>
+              <Row>
+                <h1>Free for your whole team</h1>
+              </Row>
+              <Row>
+                <h1>
+                  For individuals or teams looking to organize any project.
+                </h1>
+              </Row>
+              <Row>
+                <div>
+                  <Button>Get Started</Button>
+                </div>
+              </Row>
+              <Row classname="row-border-top">
+                <h3>INCLUDED IN FREE:</h3>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited cards</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Up to 10 boards per Workspace</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited storage (10MB/file)</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>250 Workspace command runs per month</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Custom backgrounds & stickers</p>
+                </Col>
+              </Row>
+              <Row classname="row-border-top">
+                <Col xs={1} md={1}>
+                  <p>✓</p>
+                </Col>
+                <Col xs={1} md={11}>
+                  <p>Unlimited activity log</p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+        <div className="spacer"></div>
+        <div className="spacer"></div>
       </section>
+
       <LoginFooter colorMode={colorMode} />
     </>
   );
