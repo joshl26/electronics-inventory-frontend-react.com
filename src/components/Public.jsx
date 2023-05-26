9bddd372091bfe906b8e4fbc9353abde76bc303d
Joshua Lehman <joshlehman.dev@gmail.com>
2023-05-26 18:12:22 -0400

    style_updates

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -8,8 +8,11 @@ import SwipeUpIcon from "../svg/SwipeUpIcon.json";
 import Lottie from "lottie-react";
 import CustomerGallery from "../features/pages/CustomerGallery";
 import { useEffect, useCallback } from "react";
+import HamburgerIcon from "../svg/HamburgerMenu.json";
 
 const Public = () => {
+  const [loading, setLoading] = useState(true);
+
   const [colorMode, setColorMode] = useState(
     JSON.parse(localStorage.getItem("colorMode"))
   );
@@ -38,6 +41,17 @@ const Public = () => {
   };
 
   useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      console.log("This will run after 1 second!");
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  useEffect(() => {
     const colorMode = JSON.parse(localStorage.getItem("colorMode"));
 
     if (colorMode === null) {
@@ -46,7 +60,15 @@ const Public = () => {
     }
   }, [colorMode, publicStyle]);
 
-  const content = (
+  const content = loading ? (
+    <section className={publicStyle}>
+      <Lottie
+        className="hamburger-icon"
+        animationData={HamburgerIcon}
+        loop={true}
+      />
+    </section>
+  ) : (
     <section className={publicStyle}>
       <Lottie
         onClick={SwipeUpClickHandler}
